faf6cfaa6b69f31d41cdbc349fcc256ce707b3bb
siser879 <77883892+siser879@users.noreply.github.com>
2021-02-02 15:53:12 +0100

    Add files via upload

---
+++ funcionalidad.php
@@ -0,0 +1,15 @@
+<?php
+	$pokemon = $_POST['pokemon'];
+	$n = $_POST['n'];
+	$k = $_POST['k'];
+
+	switch($pokemon){
+		case 1:
+		echo "La probabilida de que ese Bulbasur sea shiny es: ". 1/472;
+		break;
+		
+		case 
+		
+	}
+
+?>
